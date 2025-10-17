943d08c22b7e13a3218b5a503c6a4ac43b0f7c85
thejaswee shivakoti <shivakotithejaswee@gmail.com>
2025-10-17 12:39:34 +0530

    Add files via upload

---
+++ dashboard_routes.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, render_template
+dashboard_bp = Blueprint('dashboard', __name__, template_folder='../templates')
+
+@dashboard_bp.route('/')
+def dashboard():
+    return render_template('dashboard.html')
